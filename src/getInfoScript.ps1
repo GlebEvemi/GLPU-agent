9b2e771e5586ce204edc97063215eb54ba4deefb
GlebEvemi <gleb2466@gmail.com>
2026-05-11 14:58:10 +0300

    script change

--- src/getInfoScript.ps1
+++ src/getInfoScript.ps1
@@ -1,42 +1,37 @@
-# Собираем базовую инфу
-$info = Get-ComputerInfo
+# =========================
+# COMPUTER INFO COLLECTION
+# =========================
 
-$ramBytes = $info.CsTotalPhysicalMemory
-$ramGB = [math]::Round($ramBytes / 1GB, 2)
+# Basic computer info
+$computerInfo = Get-ComputerInfo
 
+# Processor info
+$cpu = Get-CimInstance Win32_Processor | Select-Object -First 1
 
-$disks = Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3" | ForEach-Object {
-    [PSCustomObject]@{
-        Drive       = $_.DeviceID
-        TotalSizeGB      = [math]::Round($_.Size / 1GB, 2)
-        FreeGB      = [math]::Round($_.FreeSpace / 1GB, 2)
-        UsedGB      = [math]::Round(($_.Size - $_.FreeSpace) / 1GB, 2)
-    }
+# Disk info (берем системный диск)
+$disk = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'"
+
+# RAM conversion
+$totalRamGb = [math]::Round($computerInfo.CsTotalPhysicalMemory / 1GB, 2)
+
+# Disk conversion
+$diskTotalGb = [math]::Round($disk.Size / 1GB, 2)
+$diskFreeGb = [math]::Round($disk.FreeSpace / 1GB, 2)
+
+# Create object
+$result = @{
+    Hostname          = $computerInfo.CsName
+    OSName            = $computerInfo.OsName
+    OSVersion         = $computerInfo.OsVersion
+    BiosManufacturer  = $computerInfo.BiosManufacturer
+    BiosStatus        = $computerInfo.BiosStatus
+    CsDomain          = $computerInfo.CsDomain
+    CsModel           = $computerInfo.CsModel
+    csProcessorName   = $cpu.Name
+    totalRam_Gb       = "$totalRamGb GB"
+    diskTotalSize     = "$diskTotalGb GB"
+    diskFreeGb        = "$diskFreeGb GB"
 }
 
-# Формируем объект (оставь только нужные поля)
-$result = [PSCustomObject]@{
-    Hostname            = $env:COMPUTERNAME
-    OSName              = $info.OsName
-    OSVersion           = $info.OsVersion
-    BiosManufacturer    = $info.BiosManufacturer
-    BiosFirmwareType    = $info.BiosFirmwareType
-    BiosVersion         = $info.BiosBIOSVersion
-    BiosStatus          = $info.BiosStatus
-    CsBootupState       = $info.CsBootupState
-    CsDNSHostname       = $info.CsDNSHostname
-    CsDomain            = $info.CsDomain
-    CsDomainRole        = $info.CsDomainRole
-    CsManufacturer      = $info.CsManufacturer
-    CsModel             = $info.CsModel
-    CsName              = $info.CsName
-    CsNumberOfLogicalProcessors = $info.CsNumberOfLogicalProcessors
-    CsNumberOfProcessors = $info.CsNumberOfProcessors
-    CsProcessors        = $info.CsProcessors
-    CsStatus            = $info.CsStatus
-    TotalRAM_GB         = $ramGB
-    
-    Disks               = $disks
-}
-
+# Convert to JSON
 $result | ConvertTo-Json -Depth 3
